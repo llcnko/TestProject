76db25586e30f052f21e86acf7bc032e6ad91c15
llcnko <llcnko@list.ru>
2022-06-16 22:29:15 +0300

    Updated basic test data and Search page class

--- Conf/config.py
+++ Conf/config.py
@@ -1,5 +1,7 @@
 class TestData:
+
     CHROME_DRIVER_EXEC_PATH = "C:/Users/lyubov/PycharmProjects/TestTask"
     START_URL = "http://google.com"
+    CALC_INVOKE = "Калькулятор"
     CALC_EXPRESSION = "1 × 2 - 3 + 1 ="
     RESULT = "0"
--- WebPages/SearchPage.py
+++ WebPages/SearchPage.py
@@ -6,3 +6,4 @@ from WebPages.Page import Page
 class SearchPage(Page):
 
     SEARCH_FIELD = (By.NAME, "q")
+    SEARCH_BUTTON = (By.NAME, "btnK")
