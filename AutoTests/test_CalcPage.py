ce72a0b56905c4c5e78a40d928cf62e77cfedb27
llcnko <llcnko@list.ru>
2022-06-22 19:33:48 +0300

    Refactored tests block

--- AutoTests/test_CalcPage.py
+++ AutoTests/test_CalcPage.py
@@ -7,20 +7,24 @@ from WebPages.CalcPage import CalcPage
 
 class TestCalcPage(ParentTest):
 
+    """SEARCH_FIELD = (By.NAME, "q")
+    SEARCH_BUTTON = (By.NAME, "btnK")
+    CALC_TEXT_BOX = (By.CLASS_NAME, "jlkklc")"""
+
     def test_memory_string(self):
         self.calcPage = CalcPage(self.driver)
-        self.driver.find_element(By.NAME, "q").send_keys(TestData.WORD_CALC)
-        self.driver.find_element(By.NAME, "btnK").click()
-        self.driver.find_element(By.CLASS_NAME, "jlkklc").send_keys(TestData.CALC_EXPRESSION)
-        self.driver.find_element(By.CLASS_NAME, "jlkklc").send_keys(Keys.ENTER)
+        self.driver.find_element(CalcPage.SEARCH_FIELD).send_keys(TestData.WORD_CALC)
+        self.driver.find_element(CalcPage.SEARCH_BUTTON).click()
+        self.driver.find_element(CalcPage.CALC_TEXTBOX).send_keys(TestData.CALC_EXPRESSION)
+        self.driver.find_element(CalcPage.SEARCH_FIELD).send_keys(Keys.ENTER)
         # force page up as page scrolls down for some reason
-        self.driver.find_element(By.ID, "logo").send_keys(Keys.CONTROL + Keys.HOME)
-        mmr_str = self.driver.find_element(By.CLASS_NAME, "vUGUtc").text
-        assert mmr_str == TestData.CALC_EXPRESSION + " =", 'Memory string is correct'
-        assert 1 == 1, 'incorrect'
+        self.driver.find_element(CalcPage.GOOGLE_LOGO).send_keys(Keys.CONTROL + Keys.HOME)
+        mmr_str = self.driver.find_element(CalcPage.MEMORY_STR).text
+        assert mmr_str == TestData.CALC_EXPRESSION + " =", 'Memory string IS correct'
+        assert 1 == 1, 'Memory string is NOT correct'
 
     def test_calc_result(self):
         # self.calcPage = CalcPage(self.driver)
-        reslt = self.driver.find_element(By.CLASS_NAME, "qv3Wpe").text
-        assert reslt == TestData.RESULT, 'Result is equal to zero'
-        assert 1 == 1, 'incorrect'
+        reslt = self.driver.find_element(CalcPage.RESULT_FIELD).text
+        assert reslt == TestData.RESULT, 'Result IS equal to zero'
+        assert 1 == 1, 'Result is NOT equal to zero'
--- WebPages/CalcPage.py
+++ WebPages/CalcPage.py
@@ -5,21 +5,32 @@ from WebPages.Page import Page
 
 
 class CalcPage(Page):
+
+    """Locators of Google Calculator Page"""
     SEARCH_FIELD = (By.NAME, "q")
     SEARCH_BUTTON = (By.NAME, "btnK")
     CALC_TEXTBOX = (By.CLASS_NAME, "jlkklc")
+    GOOGLE_LOGO = (By.ID, "logo")
+    MEMORY_STR = (By.CLASS_NAME, "vUGUtc")
+    RESULT_FIELD = (By.CLASS_NAME, "qv3Wpe")
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver.get(TestData.GOOGLE_URL)
 
+    """Methods of Google Calculator Page"""
+
+    """Method searches for Google Calculator"""
+    """and pushes the search button"""
     def do_search(self, search_string):
         self.do_send_key(self.SEARCH_FIELD, TestData.WORD_CALC)
         self.do_click(self.SEARCH_BUTTON)
 
+    """Method makes Google Calculator process math expression"""
+    """and returns the result"""
     def do_math(self, search_expression):
         self.do_send_key(self.CALC_TEXTBOX, TestData.CALC_EXPRESSION)
         self.do_send_key(self.CALC_TEXTBOX, Keys.ENTER)
         self.do_send_key(self.CALC_TEXTBOX)
-        result_str = self.driver.find_element(By.CLASS_NAME, "qv3Wpe").text
+        result_str = self.driver.find_element(self.RESULT_FIELD).text
         return result_str
