3a08ed32b1687bec4698f998f37cff537831fde5
llcnko <llcnko@list.ru>
2022-06-22 18:51:56 +0300

    Refactored tests

--- AutoTests/test_CalcPage.py
+++ AutoTests/test_CalcPage.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from AutoTests.ParentTest import ParentTest
@@ -18,11 +16,11 @@ class TestCalcPage(ParentTest):
         # force page up as page scrolls down for some reason
         self.driver.find_element(By.ID, "logo").send_keys(Keys.CONTROL + Keys.HOME)
         mmr_str = self.driver.find_element(By.CLASS_NAME, "vUGUtc").text
-        assert mmr_str == TestData.CALC_EXPRESSION + " ="
-        assert False, "Memory string is not equal"
+        assert mmr_str == TestData.CALC_EXPRESSION + " =", 'Memory string is correct'
+        assert 1 == 1, 'incorrect'
 
     def test_calc_result(self):
         # self.calcPage = CalcPage(self.driver)
         reslt = self.driver.find_element(By.CLASS_NAME, "qv3Wpe").text
-        assert reslt == TestData.RESULT
-        assert False, "Result string is not equal to zero"
+        assert reslt == TestData.RESULT, 'Result is equal to zero'
+        assert 1 == 1, 'incorrect'
